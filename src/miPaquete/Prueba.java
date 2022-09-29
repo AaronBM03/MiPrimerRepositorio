8803e9ff99ba5fe0c9458f6000ea3be1505c26ee
AaronBM03 <aaronblancomedrano_7231@escuelaces.net>
2022-09-29 12:30:04 +0200

    Commit de prueba primero

---
+++ src/miPaquete/Prueba.java
@@ -0,0 +1,16 @@
+package miPaquete;
+
+/**
+ * 
+ * Programa simplemente para poder hacer un commit de prueba en el primer repositorio.
+ * @author Aarón Blanco Medrano
+ *
+ */
+
+public class Prueba
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Mi nombre es: Aarón Blanco Medrano");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author aaron
+ *
+ */
+module PruebaParaRepositorioGit
+{
+}
